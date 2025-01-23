51e21ddc16705363c899634a5c61b53ffc084a49
ankithchowdary <ankithc29@gmail.com>
2025-01-24 00:05:25 +0530

    update

--- asynchronous.js
+++ asynchronous.js
@@ -9,4 +9,17 @@ console.log(data);
 //Start
 //Start learning Node Js:Hello my name is ankith.
 //Created on 1737647532860
+
+fs.readFile('./readthis.txt','utf-8',(err,data1)=>{
+  fs.readFile(`./${data1}.txt`,'utf-8',(err,data2)=>{
+    console.log(data2);
+    fs.readFile('./append.txt','utf-8',(err,data3)=>{
+      console.log(data3);
+
+      fs.writeFile('./final.txt',`${data2}`,'utf-8',err=>{
+
+      })
+      });
+    });
+  });
  console.log('Start');
